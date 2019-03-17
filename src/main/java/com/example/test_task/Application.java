988a4eece4b447f988a8f1bfadee5e7d2b57a5e0
gilyazovigor <gilyazovigorperm@gmail.com>
2019-03-17 23:33:35 +0500

    connected PostgreSQL DB to Java using JOOQ

--- src/main/java/com/example/test_task/Application.java
+++ src/main/java/com/example/test_task/Application.java
@@ -2,6 +2,7 @@ package com.example.test_task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Application {
 
